39779976efa21715fc630e17d0c4ee5cfa41f2a6
Tatiana_K <ottertut@gmail.com>
2023-11-16 11:53:27 +0200

    ip

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -31,7 +31,7 @@ const ProductList = () => {
             totalPrice: getTotalPrice(addedItems),
             queryId,
         }
-        fetch('http://localhost:8000/web-data', {
+        fetch('http://5.35.13.106:8000/web-data', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
