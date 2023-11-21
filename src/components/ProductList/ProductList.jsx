8ad34af530913e81bb05a14feca95fc20ab2bfe7
Tatiana_K <ottertut@gmail.com>
2023-11-21 14:42:55 +0200

    url

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -31,7 +31,7 @@ const ProductList = () => {
             totalPrice: getTotalPrice(addedItems),
             queryId,
         }
-        fetch('https://lechic.beauty/web-data', {
+        fetch('https://lechic.beauty:8000', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
